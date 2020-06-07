771e25bf791ab1fa6241e18341c9e646a60ba8fd
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-06-07 20:00:15 -0300

    Created setter method to cardName property on CardBase class, adjusting text name on card layout.

--- src/js/CardBase.js
+++ src/js/CardBase.js
@@ -2,10 +2,10 @@ export default class CardBase extends Phaser.GameObjects.Container {
     constructor(data) {        
         let {scene, x, y, name, card, image, depth} = data;
         let spriteCard = new Phaser.GameObjects.Sprite(scene, 0, 0, card);
-        let spriteImage = new Phaser.GameObjects.Sprite(scene, 0, 0, image);
-        console.log(data);
+        let spriteImage = new Phaser.GameObjects.Sprite(scene, 0, 20, image);
         let textName = 
             new Phaser.GameObjects.BitmapText(scene, 0, 0, 'pressstart', name, 16, Phaser.GameObjects.BitmapText.ALIGN_CENTER);
+        
         super(scene, x, y, [spriteCard, spriteImage, textName]);
 
         this.spriteCard = spriteCard;
@@ -15,5 +15,14 @@ export default class CardBase extends Phaser.GameObjects.Container {
         this.depth = depth;
         this.scene = scene;
         this.scene.add.existing(this);
+    }
+
+    set cardName(name) {
+        this._cardName = name;
+        this.textName.text = this._cardName;
+        this.textName.maxWidth = this.spriteCard.maxWidth;
+        this.textName.tint = 0;
+        this.textName.x = -this.textName.width / 2;
+        this.textName.y = 120 - this.textName.height;
     }
 }
