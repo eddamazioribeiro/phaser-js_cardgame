cd2ec29956e14e87f85d79a42bcfdc1d16aa8914
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-06-07 18:13:03 -0300

    Loading assets and font that will be used later in the game.

--- src/js/MainScene.js
+++ src/js/MainScene.js
@@ -4,6 +4,20 @@ export default class MainScene extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('card', '../assets/armor.png');
+        this.load.image('card', '../assets/card.png');
+        this.load.image('card', '../assets/dead.png');
+        this.load.image('card', '../assets/deadknight.png');
+        this.load.image('card', '../assets/firedrake.png');
+        this.load.image('card', '../assets/goldendragon.png');
+        this.load.image('card', '../assets/healingpotion.png');
+        this.load.image('card', '../assets/kobold.png');
+        this.load.image('card', '../assets/ogre.png');
+        this.load.image('card', '../assets/paladin.png');
+        this.load.image('card', '../assets/playercard.png');
+        this.load.image('card', '../assets/restartbutton.png');
+        this.load.image('card', '../assets/shield.png');
+        this.load.image('card', '../assets/troll.png');
+        this.load.bitmapFont('pressstart', '../fonts/pressstart.fnt');
     }
 };
